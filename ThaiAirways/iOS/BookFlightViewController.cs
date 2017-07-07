86846c80893f4196a31f478c83df1b3684427feb
Vijay Kumar 2 <vkumar60@sapient.com>
2017-07-07 11:03:31 +0530

    Added another controller

---
+++ ThaiAirways/iOS/BookFlightViewController.cs
@@ -0,0 +1,13 @@
+using Foundation;
+using System;
+using UIKit;
+
+namespace ThaiAirways.iOS
+{
+    public partial class BookFlightViewController : UIViewController
+    {
+        public BookFlightViewController (IntPtr handle) : base (handle)
+        {
+        }
+    }
+}
