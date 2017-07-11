101aa6fa6dd70759811d66e6da7534c073bfd9de
Vijay Kumar 2 <vkumar60@sapient.com>
2017-07-11 15:49:59 +0530

    Added icon for Android and fixes for iOS

---
+++ ThaiAirways/iOS/View/FlgihtHeaderViewCell.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+using Foundation;
+using UIKit;
+
+namespace ThaiAirways.iOS.View
+{
+    public partial class FlgihtHeaderViewCell : UITableViewCell
+    {
+        public static readonly NSString Key = new NSString("FlgihtHeaderViewCell");
+        public static readonly UINib Nib;
+
+        static FlgihtHeaderViewCell()
+        {
+            Nib = UINib.FromName("FlgihtHeaderViewCell", NSBundle.MainBundle);
+        }
+
+        protected FlgihtHeaderViewCell(IntPtr handle) : base(handle)
+        {
+            // Note: this .ctor should not contain any initialization logic.
+        }
+    }
+}
