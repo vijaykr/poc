e21734559c5b6e36dad7ce79a619cfd7f3e626b3
SAPIENT\agoe35 <agoel35@sapient.com>
2017-07-11 11:08:10 +0530

    Bottom Navigation Changes

--- ThaiAirways/Droid/FlightLandingActivity.cs
+++ ThaiAirways/Droid/FlightLandingActivity.cs
@@ -11,71 +11,74 @@ using ThaiAirways.Vo;
 using System.Threading.Tasks;
 using Plugin.Connectivity;
 using Square.Picasso;
+using Android.Support.Design.Internal;
+using Android.Util;
 
 namespace ThaiAirways.Droid
 {
-    [Activity(Label = "ThaiAirways", MainLauncher = true, Icon = "@drawable/icon", Theme ="@style/Theme.AppCompat.Light.NoActionBar")]
+    [Activity(Label = "ThaiAirways", MainLauncher = true, Icon = "@drawable/icon", Theme = "@style/Theme.AppCompat.Light.NoActionBar")]
     public class FlightLandingActivity : AppCompatActivity
     {
         String currentDate;
 
-		Product product1;
-		Product product2;
+        private Product product1;
+        private Product product2;
+        private BottomNavigationView bottomNavigaion;
 
-		protected override void OnCreate(Bundle bundle)
+        protected override void OnCreate(Bundle bundle)
         {
-			this.RequestWindowFeature(WindowFeatures.NoTitle);
+            this.RequestWindowFeature(WindowFeatures.NoTitle);
 
-			base.OnCreate(bundle);
+            base.OnCreate(bundle);
 
             // Set our view from the "main" layout resource
-            SetContentView (Resource.Layout.FlightLanding);
+            SetContentView(Resource.Layout.FlightLanding);
 
             currentDate = CrossPlatformUtils.TodayDate();
             TextView currentdateview = FindViewById<TextView>(Resource.Id.textView8);
             currentdateview.Text = currentDate;
 
-			if (CrossConnectivity.Current.IsConnected)
-			{
-				var TaskResult = Task.Run(async () =>
-				{
-					var products = await ContentModel.Instance.GetContensAsync();
-					var product_arr = products.ToArray();
-					product1 = product_arr[0];
-					product2 = product_arr[1];
-				});
+            if (CrossConnectivity.Current.IsConnected)
+            {
+                var TaskResult = Task.Run(async () =>
+                {
+                    var products = await ContentModel.Instance.GetContensAsync();
+                    var product_arr = products.ToArray();
+                    product1 = product_arr[0];
+                    product2 = product_arr[1];
+                });
 
-				Task.WaitAny(TaskResult);
+                Task.WaitAny(TaskResult);
 
                 FindViewById<TextView>(Resource.Id.promotion1_title_text_view).Text = product1.Title;
-				FindViewById<TextView>(Resource.Id.promotion1_desc_text_view).Text = product1.Description;
+                FindViewById<TextView>(Resource.Id.promotion1_desc_text_view).Text = product1.Description;
 
                 Picasso.With(this)
                        .Load(product1.Image)
                        .Into(FindViewById<ImageView>(Resource.Id.promotion1_image_view));
 
-				FindViewById<TextView>(Resource.Id.promotion2_title_text_view).Text = product2.Title;
-				FindViewById<TextView>(Resource.Id.promotion2_desc_text_view).Text = product2.Description;
+                FindViewById<TextView>(Resource.Id.promotion2_title_text_view).Text = product2.Title;
+                FindViewById<TextView>(Resource.Id.promotion2_desc_text_view).Text = product2.Description;
 
-				Picasso.With(this)
+                Picasso.With(this)
                        .Load(product2.Image)
                        .Into(FindViewById<ImageView>(Resource.Id.promotion2_image_view));
-			}
-			else
-			{
+            }
+            else
+            {
 
-				FindViewById<TextView>(Resource.Id.promotion1_title_text_view).Text = "";
-				FindViewById<TextView>(Resource.Id.promotion1_desc_text_view).Text = "";
+                FindViewById<TextView>(Resource.Id.promotion1_title_text_view).Text = "";
+                FindViewById<TextView>(Resource.Id.promotion1_desc_text_view).Text = "";
 
-				FindViewById<TextView>(Resource.Id.promotion2_title_text_view).Text = "";
-				FindViewById<TextView>(Resource.Id.promotion2_desc_text_view).Text = "";
-			}
+                FindViewById<TextView>(Resource.Id.promotion2_title_text_view).Text = "";
+                FindViewById<TextView>(Resource.Id.promotion2_desc_text_view).Text = "";
+            }
 
 
-			var bottomNavigaion = FindViewById<BottomNavigationView>(Resource.Id.bottom_navigation_view);
+            bottomNavigaion = FindViewById<BottomNavigationView>(Resource.Id.bottom_navigation_view);
             bottomNavigaion.NavigationItemSelected += (s, e) =>
             {
-                switch(e.Item.ItemId)
+                switch (e.Item.ItemId)
                 {
                     case Resource.Id.book_flight:
                         StartActivity(typeof(FlightBookingActivity));
@@ -98,7 +101,46 @@ namespace ThaiAirways.Droid
                 }
 
             };
+            SetIconSize();
 
+        }
+
+        private void SetIconSize()
+        {
+            BottomNavigationView navigationView = bottomNavigaion;
+            BottomNavigationMenuView menuView = (BottomNavigationMenuView)navigationView.GetChildAt(0);
+
+            for (int i = 0; i < menuView.ChildCount; i++)
+            {
+                BottomNavigationItemView item = (BottomNavigationItemView)menuView.GetChildAt(i);
+                item.SetShiftingMode(false);
+
+                TextView smallText = (TextView)menuView.GetChildAt(i).FindViewById(Resource.Id.smallLabel);
+                smallText.Visibility = ViewStates.Gone;
+
+                ImageView icon = (ImageView)menuView.GetChildAt(i).FindViewById(Resource.Id.icon);
+
+                var prm = icon.LayoutParameters;
+                var dsm = Resources.DisplayMetrics;
+
+                prm.Height = (int)TypedValue.ApplyDimension(ComplexUnitType.Dip, 40, dsm);
+                prm.Width = (int)TypedValue.ApplyDimension(ComplexUnitType.Dip, 40, dsm);
+
+                icon.LayoutParameters = prm;
+                ////Implement Accessibility
+                //if (i == 0)
+                //{
+                //	icon.ContentDescription = GetString(Resource.String.);
+                //}
+                //if (i == 1)
+                //{
+                //	icon.ContentDescription = GetString(Resource.String.saving);
+                //}
+                //if (i == 2)
+                //{
+                //	icon.ContentDescription = GetString(Resource.String.profile);
+                //}
+            }
         }
     }
 }
