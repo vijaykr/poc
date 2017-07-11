270ffac17e4ae93243b360ef808e8e06a8def96f
Adish <ajain7@sapient.com>
2017-07-11 13:03:41 +0530

    using OnResume

--- ThaiAirways/Droid/FlightListActivity.cs
+++ ThaiAirways/Droid/FlightListActivity.cs
@@ -11,6 +11,7 @@ using System.Collections.Generic;
 using ThaiAirways.Model.Vo;
 using ThaiAirways.Model;
 using ThaiAirways.Utils;
+using System.Threading.Tasks;
 
 namespace ThaiAirways.Droid
 {
@@ -23,8 +24,43 @@ namespace ThaiAirways.Droid
 
         DateTime departDate;
         DateTime returnDate;
+        protected override void OnResume()
+        {
+            base.OnResume();
+            
+            List<FlightSearchEntity> flightsData = new List<FlightSearchEntity>();
+            flightsData = FlighSearchModel.Instance.FlightList;
+                        
+            if (flightsData != null)
+            {
+                FindViewById<TextView>(Resource.Id.txtNumberOfFlights).Text = flightsData.Count.ToString();
 
-        protected override void OnCreate(Bundle bundle)
+                FindViewById<TextView>(Resource.Id.txtOriginToDestPlaceTop).Text = "Bangkok to Hong Kong";
+                FindViewById<TextView>(Resource.Id.txtDepDateTop).Text = CrossPlatformUtils.GetDateInDayDateMonth(departDate);
+                FindViewById<TextView>(Resource.Id.txtRetDateTop).Text = CrossPlatformUtils.GetDateInDayDateMonth(returnDate);
+
+                flightsData = FlighSearchModel.Instance.FlightList;
+
+                mRecyclerView = FindViewById<RecyclerView>(Resource.Id.recyclerView);
+
+                // Plug in the linear layout manager:
+                mLayoutManager = new LinearLayoutManager(this);
+                mRecyclerView.SetLayoutManager(mLayoutManager);
+                //mRecyclerView.GetLayoutManager().ScrollToPosition(200);
+
+                // Plug in my adapter:
+                mAdapter = new FlightRecyclerAdapter(flightsData);
+                mRecyclerView.SetAdapter(mAdapter);
+            }
+            else
+            {
+                FindViewById<TextView>(Resource.Id.txtNumberOfFlights).Text = "";
+                FindViewById<TextView>(Resource.Id.textView10).Text = "No flight found";
+            }           
+        }
+
+        
+        protected override async void OnCreate(Bundle bundle)
         {
             base.OnCreate(bundle);
             departDate = CrossPlatformUtils.GetDeaprtDate();
@@ -32,36 +68,12 @@ namespace ThaiAirways.Droid
 
             // Set our view from the "main" layout resource
             SetContentView(Resource.Layout.FlightList);
-            
-            
-            List<FlightSearchEntity> flightsData = new List<FlightSearchEntity>();
 
-
-            flightsData = FlighSearchModel.Instance.FlightList;
-            
-
-           
-            FindViewById<TextView>(Resource.Id.txtNumberOfFlights).Text = flightsData.Count.ToString();
-            // FindViewById<TextView>(Resource.Id.txtOriginToDestPlaceTop).Text = flightsData[0].DestCode.ToUpper() +" to "+ flightsData[0].ArrCode.ToUpper();
-            FindViewById<TextView>(Resource.Id.txtOriginToDestPlaceTop).Text = "Bangkok to Hong Kong";
-            FindViewById<TextView>(Resource.Id.txtDepDateTop).Text = CrossPlatformUtils.GetDateInDayDateMonth(departDate);
-            FindViewById<TextView>(Resource.Id.txtRetDateTop).Text = CrossPlatformUtils.GetDateInDayDateMonth(returnDate);
-
-			ImageView Backbutton = FindViewById<ImageView>(Resource.Id.imgBackArrowTop);
+            ImageView Backbutton = FindViewById<ImageView>(Resource.Id.imgBackArrowTop);
             Backbutton.Click += delegate {
                 Finish();
             };
-
-            mRecyclerView = FindViewById<RecyclerView>(Resource.Id.recyclerView);
-
-            // Plug in the linear layout manager:
-            mLayoutManager = new LinearLayoutManager(this);
-            mRecyclerView.SetLayoutManager(mLayoutManager);
-            //mRecyclerView.GetLayoutManager().ScrollToPosition(200);
-           
-            // Plug in my adapter:
-            mAdapter = new FlightRecyclerAdapter(flightsData);
-            mRecyclerView.SetAdapter(mAdapter);
+        
         }
     }
     
